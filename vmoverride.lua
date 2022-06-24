31c978c234c3c85ec707c0e41880240733324f64
urnotzach <40055086+urnotzach@users.noreply.github.com>
2022-06-24 00:51:13 -0500

    Update vmoverride.lua
    Added viewmodel sway offsets.

--- vmoverride.lua
+++ vmoverride.lua
@@ -1,14 +1,28 @@
-local x = 20
-local y = 4
-local z = -12
+--[[
+	Viewmodel Offset+Sway Override LUA
+	Credits to GitHub user Xee45 for the original LUA https://github.com/Xee545/lmaobox-luas/blob/main/vmoverride.lua
+	Viewmodel Offsets will not work with tf_use_min_viewmodels. This is hardcoded in TF2.
+	
+	You'll have to set the viewmodel overrides yourself. 
+]]
+
+local x = 0
+local y = 0
+local z = 0
+local sway = 0
+local interp = 0
 
 local function override()
 
   client.RemoveConVarProtection( "tf_viewmodels_offset_override" )
+  client.RemoveConVarProtection( "cl_wpn_sway_interp")
+  client.RemoveConVarProtection( "cl_wpn_sway_scale") 
   client.SetConVar( "tf_viewmodels_offset_override", x .. " " .. y .. " " .. z )
+  client.SetConVar( "cl_wpn_sway_scale", sway .. " " )
+  client.SetConVar( "cl_wpn_sway_interp", interp .. " " )
   
 end
 
 override()
-print("VM Override Loaded!")
+print("Viewmodel Override Loaded!")
 engine.PlaySound("buttons/button3.wav")
